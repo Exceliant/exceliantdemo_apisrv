249bbbbb21549ffdec87591ac140cc96edd69578
Evgen <evgeniy.struts@gmail.com>
2023-10-31 19:11:57 -0400

    Improved guard, code cleanup

--- guard.js
+++ guard.js
@@ -1,8 +1,10 @@
+const regexp = new RegExp(/^.{8}-.{4}-.{4}-.{4}-.{12}$/);
 module.exports = {
   validateIdTokenClaims: (req, res, next) => {
     if (
       req.body.idTokenClaims.sub !== undefined &&
-      req.body.idTokenClaims.sub !== ""
+      req.body.idTokenClaims.sub !== "" &&
+      regexp.test(req.body.idTokenClaims.sub) == true
     ) {
       next();
     } else {
@@ -13,7 +15,8 @@ module.exports = {
   validateIdTokenClaimsFromUrl: (req, res, next) => {
     if (
       Object.keys(req.query)[0] !== undefined &&
-      Object.keys(req.query)[0] !== ""
+      Object.keys(req.query)[0] !== "" &&
+      regexp.test(Object.keys(req.query)[0]) == true
     ) {
       next();
     } else {
